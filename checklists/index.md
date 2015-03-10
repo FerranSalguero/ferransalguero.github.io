36c981226a269c8d3bb317d79ec7afe8b2a4605c
Ferran Salguero <fsalgueromateu@gmail.com>
2015-03-10 19:43:02 +0100

    Opt-out analytics

--- checklists/index.md
+++ checklists/index.md
@@ -7,9 +7,9 @@ all_lists:
 - hiking
 ---
 
-<div class="container">
+<div class="container-fluid">
     <div class="row">
-        <div class="col-xs-12 well shadow">
+    <div class="col-xs-12 col-sm-6 well shadow">
             <h1>Checklists</h1>
             <ul class="">
                 {% for list in page.all_lists %}
@@ -17,5 +17,11 @@ all_lists:
                 {% endfor %}
             </ul>
         </div>
+
+        <div class="col-xs-12 col-sm-5 col-sm-offset-1 well shadow">
+            <h1>Other</h1>
+            <button class="btn btn-primary shadow" type="button" id="opt-button">Opt-out analytics</button>
+        </div>
     </div>
+
 </div>
--- javascripts/checklists.js
+++ javascripts/checklists.js
@@ -1,4 +1,22 @@
 $(function() {
 	$("li.box").prepend("<input type='checkbox' />").wrapInner("<label></label>");
-	;
+	$("#opt-button").click(function () {
+        gaOptout();
+	alert("done");
+    });
 });
+
+// Set to the same value as the web property used on the site
+var gaProperty = '{{ site.google_analytics_tracking_id }}';
+
+// Disable tracking if the opt-out cookie exists.
+var disableStr = 'ga-disable-' + gaProperty;
+if (document.cookie.indexOf(disableStr + '=true') > -1) {
+    window[disableStr] = true;
+}
+
+// Opt-out function
+function gaOptout() {
+    document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
+    window[disableStr] = true;
+}
